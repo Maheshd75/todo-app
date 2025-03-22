d3fac901118b23f9b95c513a08f5e323f68a4c1a
Maheshd75 <Maheshd7569@gmail.com>
2025-03-22 15:41:07 +0530

    first

--- todo_app_backend/todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from todo.views import todo_list, todo_detail
-
-urlpatterns = [
-    path('todos/', todo_list),
-    path('todos/<int:pk>/', todo_detail),
-]
