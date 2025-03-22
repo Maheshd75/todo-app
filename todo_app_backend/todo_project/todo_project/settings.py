8c47d726a14c839d163de66180d714e7072463b2
Maheshd75 <Maheshd7569@gmail.com>
2025-03-22 19:32:32 +0530

    first

--- todo_app_backend/todo_project/todo_project/settings.py
+++ todo_app_backend/todo_project/todo_project/settings.py
@@ -129,10 +129,11 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEBUG = False
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://todo-app-sooty-seven-98.vercel.app",
+#CORS_ALLOWED_ORIGINS = [""
+   # "https://todo-app-sooty-seven-98.vercel.app",
       # For local development
-]
+#]
 ALLOWED_HOSTS = ["todo-app-sooty-seven-98.vercel.app",]
 
